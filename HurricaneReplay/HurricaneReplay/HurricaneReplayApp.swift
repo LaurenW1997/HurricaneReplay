47fc84524f1b1895183b6ecacc86b3351035edd7
ywan245 <ywan245@lsu.edu>
2025-11-03 08:56:18 -0600

    Init project

---
+++ HurricaneReplay/HurricaneReplay/HurricaneReplayApp.swift
@@ -0,0 +1,33 @@
+//
+//  HurricaneReplayApp.swift
+//  HurricaneReplay
+//
+//  Created by COM on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct HurricaneReplayApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ HurricaneReplay/HurricaneReplayTests/HurricaneReplayTests.swift
@@ -0,0 +1,17 @@
+//
+//  HurricaneReplayTests.swift
+//  HurricaneReplayTests
+//
+//  Created by COM on 11/3/25.
+//
+
+import Testing
+@testable import HurricaneReplay
+
+struct HurricaneReplayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
